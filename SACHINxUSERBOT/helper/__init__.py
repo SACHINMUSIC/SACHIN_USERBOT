f15f378890023728bd77b51999ee07f2db5e8127
SANATANIxTECH <sanatanixtech@gmail.com>
2024-08-29 10:57:45 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHINxUSERBOT/helper/__init__.py
+++ SACHINxUSERBOT/helper/__init__.py
@@ -1,1 +1,14 @@
+import os
+import sys
+from pyrogram import Client
 
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "SACHINxUSERBOT"])
+
+async def join(client):
+    try:
+        await client.join_chat("All_SANATANI_BOT")
+    except BaseException:
+        pass
