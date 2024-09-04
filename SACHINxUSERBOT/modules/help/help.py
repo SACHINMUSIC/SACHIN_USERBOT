efbd0b844e3b107e10e3245e01551b682f34d5b4
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-04 13:31:56 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHINxUSERBOT/modules/help/help.py
+++ SACHINxUSERBOT/modules/help/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @hades_wizard × @hades_wizard •",
+                f"```{str(ac)}```\n• @V_VIP_OWNER × @Il_4ST_FIGHTER_lI •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @hades_wizard"
+            this_command += "© @V_VIP_OWNER"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -80,12 +80,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "• 🇸ᴀ ɴ ᴀ ᴛ ᴀ ɴ ɪ ₰ •"
+        ac.title = "SACHINxUSERBOT-UserBot Plugins"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @hades_wizard × @hades_wizard •"
+            message, f"```{str(ac)}```\n• @V_VIP_OWNER × @Il_4ST_FIGHTER_lI •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @hades_wizard"
+            this_command += "© @V_VIP_OWNER"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
